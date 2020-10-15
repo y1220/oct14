f29939b48478e20d634710d02b3d9d44cceba46c
Your Name <ga.2.y1220@gmail.com>
2020-10-15 14:47:48 +0200

    added user database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_152008) do
+ActiveRecord::Schema.define(version: 2020_10_15_123300) do
 
   create_table "meals", force: :cascade do |t|
     t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
