fe62f40c39af8de37b964efae616a16584cac478
Your Name <ga.2.y1220@gmail.com>
2020-10-20 18:15:26 +0200

    added link on the meal page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   def new
-    @comment= Comment.new()
+    @comment= Comment.new
   end
 
   def create
-
+    @comment= Comment.new(message: params[:message])
+    @comment.save
+    @meal = Meal.find_by(id: params[:id])
   end
 end
