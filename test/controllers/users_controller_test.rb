ef5685a691deb0e4b479dc7ccfe118200e43d4b2
Your Name <ga.2.y1220@gmail.com>
2020-10-30 17:52:16 +0100

    added few methods in controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
-    assert_response :success
+    get users_path
+    #assert_response :success
+    #assert_redirected_to path
+    assert_redirected_to login_path
+  end
+
+  test "should get index2" do
+    get users_path
+    #logined user should be routed here though...
+    assert_redirected_to meals_path
   end
 
 end
