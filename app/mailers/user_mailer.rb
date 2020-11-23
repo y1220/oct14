73f1b2c1d8c4f430ea6255901a0553ac05c8a8f3
y1220 <ga.2.y1220@gmail.com>
2020-11-23 15:08:37 +0100

    added image in a mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url= 'http://localhost:3000/login'
+    attachments.inline['cook.png'] = File.read(Rails.public_path.join('cook.png'))
     mail(to: @user.email, subject: 'Welcome Happy Recipe Site')
   end
 
